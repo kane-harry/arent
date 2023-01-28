075a1802f94ca42a6a3e808552f9b3e6b33ddcfe
a <a>
2023-01-28 23:56:55 +0700

    init

--- src/components/action/TodayActionButton.tsx
+++ src/components/action/TodayActionButton.tsx
@@ -1,10 +1,11 @@
+import './TodayAction.css'
 import actionMorning from './action_morning.svg'
 import actionLunch from './action_lunch.svg'
 import actionDinner from './action_dinner.svg'
 import actionSnack from './action_snack.svg'
 export default function TodayActionButton() {
     return (
-        <div className={'flex mx-96 my-6 justify-between'}>
+        <div className={'d-flex justify-content-between today-action'}>
             <img src={actionMorning}/>
             <img src={actionLunch}/>
             <img src={actionDinner}/>
