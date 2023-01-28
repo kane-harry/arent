2f593f4906252d3db889bc8fcad9540c25cb20a9
a <a>
2023-01-29 00:25:56 +0700

    init

--- src/components/statistic/TodayStatistic.tsx
+++ src/components/statistic/TodayStatistic.tsx
@@ -2,18 +2,20 @@ import './TodayStatistic.css'
 import icon from './d01.png'
 import spinner from './spinner.svg'
 import moment from 'moment'
-
+const formatNumber = function (date:any) {
+    return date > 9 ? date : `0${date}`
+}
 export default function TodayStatistic() {
     const today = moment()
     const month = today.format('M');
     const day   = today.format('D');
-    const percent = '24%'
+    const percent = Math.floor(Math.random() * 100)
     return (
         <div className={"today-statistic self-center"}>
             <img src={icon} className="absolute left-0"/>
             <img src={spinner} className={"cursor-pointer absolute top-0 left-0 mt-16 ml-40 hover:shadow-outline circle"}/>
-            <label className={"absolute month-day"}>{`${month}/${day}`}</label>
-            <label className={"absolute percent"}>{percent}</label>
+            <label className={"absolute month-day"}>{`${formatNumber(month)}/${formatNumber(day)}`}</label>
+            <label className={"absolute percent"}>{`${formatNumber(percent)}%`}</label>
         </div>
     )
 }
