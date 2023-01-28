fbb99c995fb4d6a979ac507353f67ee73d20abf7
a <a>
2023-01-29 00:39:23 +0700

    init

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -9,48 +9,47 @@ import iconChallenge from './icon_challenge.svg'
 import iconLogo from './logo.svg'
 
 function Header() {
-  const { totalCount } = useSelector((state:any) => state.cart)
+    const { totalCount } = useSelector((state:any) => state.cart)
 
-  return (
-      <div className="d-flex flex-column flex-md-row align-items-center mb-3 border-bottom header justify-content-between align-items-center">
-        <img src={iconLogo} />
-        <Link
-            to="/"
-            className="d-flex align-items-center text-decoration-none header-link"
-        >
-          <img src={iconChallenge} className="header-image" />
-          <span className="">チャレンジ</span>
-        </Link>
+    return (
+        <div className="d-flex flex-column flex-md-row align-items-center mb-3 border-bottom header justify-content-between align-items-center">
+            <img src={iconLogo} />
 
-        <Link
-            to="/"
-            className="d-flex align-items-center text-decoration-none header-link"
-        >
-          <img src={iconMyRecord} className="header-image"/>
-          <span className="">自分の記録</span>
-        </Link>
+            <nav className="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+                <Link
+                    to="/"
+                    className="d-flex align-items-center text-decoration-none header-link"
+                >
+                    <img src={iconChallenge} className="header-image" />
+                    <span className="">チャレンジ</span>
+                </Link>
+                <Link
+                    to="/"
+                    className="d-flex align-items-center text-decoration-none header-link"
+                >
+                    <img src={iconMyRecord} className="header-image"/>
+                    <span className="">自分の記録</span>
+                </Link>
+                <span className="cart py-2 text-decoration-none position-relative">
+                    <Link to="/cart" className="header-link">
+                        <img src={iconInfo} />
+                        <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                            {totalCount}
+                            <span className="visually-hidden">unread messages</span>
+                        </span>
+                    </Link>
+                </span>
+            </nav>
+            <span className="header-link icon-info">自分の記録</span>
 
-        <nav className="d-inline-flex mt-2 mt-md-0 ms-md-auto">
-          <span className="cart py-2 text-decoration-none position-relative">
-            <Link to="/cart" className="header-link">
-              <img src={iconInfo} />
-              <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                {totalCount}
-                <span className="visually-hidden">unread messages</span>
-              </span>
+            <Link
+                to="/"
+                className="d-flex align-items-center text-decoration-none"
+            >
+                <img src={iconMenu} />
             </Link>
-          </span>
-        </nav>
-        <span className="header-link icon-info">自分の記録</span>
-
-        <Link
-            to="/"
-            className="d-flex align-items-center text-decoration-none"
-        >
-          <img src={iconMenu} />
-        </Link>
-      </div>
-  )
+        </div>
+    )
 }
 
 export default Header
